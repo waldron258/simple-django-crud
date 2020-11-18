8b14fda44ffb20fba42e0988c9708bd596a8f0fd
Luis Garcia <waldronl@uninorte.edu.co>
2020-11-18 11:43:40 -0500

    Heroku deployment5

--- user_example/views.py
+++ user_example/views.py
@@ -77,7 +77,7 @@ def updateCar(request, pk):
         if form.is_valid():
             form.save()
             return redirect('/car-list')
-    return render(request, 'templates/user_example/car_update.html', {'car': car})
+    return render(request, 'user_example/car_update.html', {'car': car})
    
 
 # Car list
